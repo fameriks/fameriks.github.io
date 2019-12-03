b7f44d4aac19e614ab88391b5f17f1af2ff0706b
fameriks <>
2019-12-03 09:20:21 -0500

    ignore ts temporary

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore-line
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore-line
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
